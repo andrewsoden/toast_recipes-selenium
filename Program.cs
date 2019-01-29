12f1d0feda42dc544c345904b6c61008c1aa5f66
Andrew Soden <andrewsoden@hotmail.com>
2019-01-29 20:03:23 +1000

    Initial selenium skeleton

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using NUnit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+[TestFixture]
+public class TestProgram
+{
+    private IWebDriver driver;
+
+    [SetUp]
+    public void SetupTest()
+    {
+        IWebDriver WebDriver = new ChromeDriver(@"./bin/drivers/");
+        driver = new ChromeDriver();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        driver.Url ="https://andrewsoden.github.io/toast_recipes/#/";
+    }
+
+    [TearDown]
+    public void TeardownTest()
+    {
+        driver.Quit();
+    }
+
+    [Test]
+    public void testMethod()
+    {
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # toast_recipes-selenium
+
+
+VS Code Plugins
+1. C# for Visual Studio Code (powered by OmniSharp)
+2. .NET Core Test Explorer
+
+Download and extract latest ChromeDriver
+From: http://chromedriver.chromium.org/
+Into: /bin/drivers
