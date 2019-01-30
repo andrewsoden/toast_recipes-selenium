f26f848cf5045a711fdffca65103baa755d30e3b
Andrew Soden <andrewsoden@hotmail.com>
2019-01-30 18:55:12 +1000

    Chromedriver path attempted fix

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ public class TestProgram
     [SetUp]
     public void SetupTest()
     {
-        IWebDriver WebDriver = new ChromeDriver(@"./bin/drivers/");
+        //IWebDriver driver = new ChromeDriver(@"C:\my\path\to\chromedriver\directory");
+        IWebDriver driver = new ChromeDriver("/users/andrewsoden/Desktop/Andrew/git/toast_recipes-selenium/bin/drivers/");
         driver = new ChromeDriver();
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
         driver.Url ="https://andrewsoden.github.io/toast_recipes/#/";
