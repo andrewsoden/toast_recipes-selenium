6518a06cd58fe5f0fc5e3c490d21002441c8f0a1
Andrew Soden <andrewsoden@hotmail.com>
2019-02-13 18:45:44 +1000

    Refactor tests

--- SectionNavigation.cs
+++ SectionNavigation.cs
@@ -24,30 +24,34 @@ namespace Test_SectionNavigation
             driver.Url = TestProperties.websiteURL;
         }
 
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
         [Test]
         public void SectionNavigation()
         {
 
-            IWebElement header = driver.FindElement(By.ClassName("title"));
+            var header = driver.FindElement(By.ClassName("title"));
             Assert.True(header.Displayed);
 
-            IWebElement homeNavigation = driver.FindElement(By.PartialLinkText("Home"));
+            var homeNavigation = driver.FindElement(By.PartialLinkText("Home"));
             homeNavigation.Click();
             Assert.True(driver.FindElement(By.XPath("//*[@id='root']/div/div[2]/div/div/h2")).Text.Contains("Rejoice for this is the Home page")); 
 
-            IWebElement informationNavigation = driver.FindElement(By.PartialLinkText("Information"));
+            var informationNavigation = driver.FindElement(By.PartialLinkText("Information"));
             informationNavigation.Click();
             Assert.True(driver.FindElement(By.XPath("//*[@id='root']/div/div[2]/div/div/h2")).Text.Contains("Rejoice for this is the Information page"));
 
-            IWebElement contactNavigation = driver.FindElement(By.PartialLinkText("Contact"));
+            var contactNavigation = driver.FindElement(By.PartialLinkText("Contact"));
             contactNavigation.Click();
             Assert.True(driver.FindElement(By.XPath("//*[@id='root']/div/div[2]/div/div/h1")).Text.Contains("Contact"));
 
-            IWebElement loginNavigation = driver.FindElement(By.PartialLinkText("Login"));
+            var loginNavigation = driver.FindElement(By.PartialLinkText("Login"));
             loginNavigation.Click();
             Assert.True(driver.FindElement(By.XPath("//*[@id='root']/div/div[2]/div/div/h2")).Text.Contains("Rejoice for this is the Login page"));
-        
-            driver.Quit();
         }
     }
 }
--- SubmitContactForm.cs
+++ SubmitContactForm.cs
@@ -26,36 +26,31 @@ namespace Test_SubmitContactForm
             driver.Url = TestProperties.websiteURL;
         }
 
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
         [Test]
         public void SubmitContactForm()
         {   
-            IWebElement contactNavigation = driver.FindElement(By.PartialLinkText("Contact"));
-            contactNavigation.Click();
+            driver.FindElement(By.PartialLinkText("Contact")).Click();
 
-            IWebElement firstNameField = driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(1) > input"));
-            firstNameField.Click();
-            firstNameField.Clear();
-            firstNameField.SendKeys("Doctor");
+            driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(1) > input"))
+            .SendKeys("Doctor");
 
-            IWebElement surnameField = driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(2) > input"));
-            surnameField.Click();
-            surnameField.Clear();
-            surnameField.SendKeys("Tester");
+            driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(2) > input"))
+            .SendKeys("Tester");
 
-            IWebElement emailField = driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(3) > input"));
-            emailField.Click();
-            emailField.Clear();
-            emailField.SendKeys("thetester@test.com.au");
+            driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(3) > input"))
+            .SendKeys("thetester@test.com.au");
 
-            IWebElement passwordField = driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(4) > input"));
-            passwordField.Click();
-            passwordField.Clear();
-            passwordField.SendKeys("secret2019");
+            driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(4) > input"))
+            .SendKeys("secret2019");
 
-            IWebElement reEnterPasswordField = driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(5) > input"));
-            reEnterPasswordField.Click();
-            reEnterPasswordField.Clear();
-            reEnterPasswordField.SendKeys("secret2019");
+            driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(5) > input"))
+            .SendKeys("secret2019");
 
             // TODO - Does not select radio button
             IWebElement contactMethodField = driver.FindElement(By.Id("contactEmail"));
@@ -64,18 +59,14 @@ namespace Test_SubmitContactForm
             SelectElement foundUsField = new SelectElement(driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(7) > select")));
             foundUsField.SelectByText("Friends/family");
 
-            IWebElement commentField = driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(8) > textarea"));
-            commentField.Click();
-            commentField.Clear();
-            commentField.SendKeys("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.!?@0123456789");
+            driver.FindElement(By.CssSelector("#root > div > div:nth-child(2) > div > div > form > div > div > div:nth-child(8) > textarea"))
+            .SendKeys("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.!?@0123456789");
 
             IWebElement submitButton = driver.FindElement(By.Id("submitButton"));
             Actions actions = new Actions(driver);
             actions.MoveToElement(submitButton);
             actions.Perform();
             submitButton.Submit();
-
-            driver.Quit();
         }
     }
 }
